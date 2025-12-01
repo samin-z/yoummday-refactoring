45425a4671d3a76ffc2403c2970ff21f9f713774
samin-z <55046964+samin-z@users.noreply.github.com>
2025-12-01 17:47:20 +0100

    add test structure for PermissionHandler

--- Test/Handler/PermissionHandlerTest.php
+++ Test/Handler/PermissionHandlerTest.php
@@ -4,9 +4,42 @@ declare(strict_types=1);
 
 namespace Test\Handler;
 
+use App\Handler\PermissionHandler;
 use PHPUnit\Framework\TestCase;
+use ProgPhil1337\SimpleReactApp\HTTP\Response\JSONResponse;
+use ProgPhil1337\SimpleReactApp\HTTP\Routing\RouteParameters;
+use Psr\Http\Message\ServerRequestInterface;
 
 class PermissionHandlerTest extends TestCase
 {
-    // TODO
+    private PermissionHandler $handler;
+    private ServerRequestInterface $serverRequest;
+
+    protected function setUp(): void
+    {
+        $this->handler = new PermissionHandler();
+        $this->serverRequest = $this->createMock(ServerRequestInterface::class);
+    }
+    
+    public function testTokenWithReadPermissionReturnsTrue(): void
+    {
+        // TODO: Implement test for token1234 (read permission)
+    }
+
+    public function testTokenReadonlyWithReadPermissionReturnsTrue(): void
+    {
+        // TODO: Implement test for tokenReadonly (read permission)
+    }
+
+
+    public function testInvalidTokenReturnsFalse(): void
+    {
+        // TODO: Implement test for invalid (non-exsistent)token
+    }
+
+   
+    public function testMissingTokenParameterReturnsFalse(): void
+    {
+        // TODO: Implement test for missing token
+    }
 }
